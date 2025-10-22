097d972492e0dadcb6dc3f4680276a5c687d0685
Andrey <andreymatheus92@gmail.com>
2025-10-21 21:30:20 -0300

    feat: integracao com back feita

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,32 +7,37 @@ import { JwtAuthGuard } from './common/guards/roles.guard';
 import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
 
 async function bootstrap() {
-    dotenv.config();
-    const app = await NestFactory.create(AppModule);
+  dotenv.config();
+  const app = await NestFactory.create(AppModule);
 
-    const reflector = app.get(Reflector);
-    app.useGlobalGuards(new JwtAuthGuard(reflector));
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
-    app.useGlobalPipes(
-        new ValidationPipe({
-            whitelist: true,
-            forbidNonWhitelisted: true,
-            transform: true,
-        }),
-    );
+  const reflector = app.get(Reflector);
+  app.useGlobalGuards(new JwtAuthGuard(reflector));
 
-    // Configuração do Swagger
-    const config = new DocumentBuilder()
-        .setTitle('Meu Carro de Linha API')
-        .setDescription('API para gerenciar passageiros, motoristas, anúncios, rotas e chat')
-        .setVersion('1.0')
-        .addBearerAuth()
-        .build();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
+  const config = new DocumentBuilder()
+    .setTitle('Meu Carro de Linha API')
+    .setDescription('API para gerenciar passageiros, motoristas, anúncios, rotas e chat')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
 
-    await app.listen(process.env.PORT || 3000);
-    //console.log(`Aplicação rodando em http://localhost:${process.env.PORT || 3000}/api`);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(process.env.PORT || 3000);
+//   console.log(`Aplicação rodando em http://localhost:${process.env.PORT || 3000}`);
 }
 bootstrap();
